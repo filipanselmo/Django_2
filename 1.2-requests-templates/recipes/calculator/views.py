2ec9fb3cbd576a7092e92ba94cc7b026f3a8c691
Artem Nedvigin <filipanselmo@yandex.ru>
2022-10-03 09:48:04 +0300

    Initial commit

---
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
+
+DATA = {
+    'omlet': {
+        'яйца, шт': 2,
+        'молоко, л': 0.1,
+        'соль, ч.л.': 0.5,
+    },
+    'pasta': {
+        'макароны, г': 0.3,
+        'сыр, г': 0.05,
+    },
+    'buter': {
+        'хлеб, ломтик': 1,
+        'колбаса, ломтик': 1,
+        'сыр, ломтик': 1,
+        'помидор, ломтик': 1,
+    },
+    # можете добавить свои рецепты ;)
+}
+
+def calculate_recipe_view(request, recipe_name):
+
+    if recipe_name in DATA:
+        data = DATA[recipe_name]
+        # servings = request.Get.get('servings', None)
+        servings = request.GET.get('servings', None)
+
+        if servings:
+            result = dict()
+            for item, value in data.items():
+                new_value = value * int(servings)
+                result[item] = new_value
+            context = {
+                'recipe_name': recipe_name,
+                'recipe': result
+            }
+        else:
+            context = {
+                'recipe_name': recipe_name,
+                'recipe': data
+            }
+    else:
+        context = None
+
+    return render(request, template_name='calculator/index.html', context=context)
+
+def home_view(request):
+    all_recipes = list(DATA.keys())
+    context = {'all_recipes': all_recipes}
+
+    return render(request, template_name='home/home.html', context=context)
+#def home_page(request,dish):
+    #template_name = 'calculator/index.html'
+    #if request.GET.get('servings') is not None:
+        #res = int(request.GET.get('servings'))
+        #recipes = {}
+        #for ind, amount in DATA[dish].items():
+            #amount_= amount * res
+            #recipes.setdefault(ind, amount_)
+        #context = {'dish': recipes}
+    #else:
+        #context = {'dish': DATA[dish]}
+
+    #return render(request,template_name=template_name,context=context)
+    #return render(request, 'calculator/index.html', context)
+# Напишите ваш обработчик. Используйте DATA как источник данных
+# Результат - render(request, 'calculator/index.html', context)
+# В качестве контекста должен быть передан словарь с рецептом:
+# context = {
+#   'recipe': {
+#     'ингредиент1': количество1,
+#     'ингредиент2': количество2,
+#   }
+# }
---
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -0,0 +1,24 @@
+"""recipes URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.urls import path
+from calculator.views import calculate_recipe_view, home_view
+
+urlpatterns = [ path('recipes/', home_view, name='home'),
+    path('recipes/<recipe_name>/', calculate_recipe_view, name='calculate_recipe'),
+    #path('recipes/<dish>/', home_page)
+    # здесь зарегистрируйте вашу view-функцию
+]
